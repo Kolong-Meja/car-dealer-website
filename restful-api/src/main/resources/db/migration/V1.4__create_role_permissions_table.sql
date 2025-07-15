698dd1b3eee7c59cae60757f78c4bf1665e56660
Faisal Ramadhan <faisalramadhan1299@gmail.com>
2025-07-15 18:26:01 +0700

    fix: fixing some issues.

--- restful-api/src/main/resources/db/migration/V1.4__create_role_permissions_table.sql
+++
@@ -1,8 +0,0 @@
-create table role_permissions (
-  role_id varchar(20) not null,
-  permission_id varchar(20) not null,
-  
-  constraint pk_role_permissions primary key(role_id, permission_id),
-  constraint fk_role_id foreign key (role_id) references roles(id) on delete cascade on update cascade,
-  constraint fk_permission_id foreign key (permission_id) references permissions(id) on delete cascade on update cascade
-);
--- restful-api/src/main/resources/db/migration/V1.5__create_user_roles_table.sql
+++
@@ -1,8 +0,0 @@
-create table user_roles (
-  user_id varchar(20) not null,
-  role_id varchar(20) not null,
-  
-  constraint pk_user_roles primary key(user_id, role_id),
-  constraint fk_user_id foreign key (user_id) references users(id) on delete cascade on update cascade,
-  constraint fk_role_id foreign key (role_id) references roles(id) on delete cascade on update cascade
-);
